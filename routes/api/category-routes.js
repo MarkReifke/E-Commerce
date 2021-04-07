d4b745a15a4d7524a44c2634148ce3dbf18f1d48
Mark Reifke <mark.reifke@gmail.com>
2021-04-07 09:38:58 -0700

    copied over all files from other VS code I made.

---
+++ routes/api/category-routes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      include: [
+        { model: Product }
+      ],
+    });
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const oneCategory = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    })
+    if (!oneCategory) {
+      res.status(404).json({ message: 'No Category was found with that ID!' });
+      return;
+    }
+    res.status(200).json(oneCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+  try {
+    const createCategory = await Category.create(req.body);
+    res.status(200).json(createCategory);
+  } catch (err) {
+    res.status(400).json(err)
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updateCategory) {
+      res.status(404).json({message: 'No Category was found with that ID!'});
+      return;
+    }
+    res.status(200).json(updateCategory); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deleteCategory) {
+      res.status(404).json({ message: 'No Category was found with that ID!' });
+      return;
+    }
+    res.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
